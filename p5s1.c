bd9e06e6cbea9339c76504add3963aa2730e541e
sugandhigunpal-debug <sugandhigunpal@gmail.com>
2025-10-10 17:14:43 +0530

    Add files via upload

---
+++ p5s1.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+
+int max ( int *a, int *b, int *c){
+    if(*a>=*b && *a>=*c){
+    return *a;
+}
+
+else if (*b>=*a && *b>=*c){
+return *b;
+}
+else{
+    return *c;
+}   
+
+} 
+  int main () {
+    int a,b,c;
+    print(" enter the numbers :");
+    scanf("%d,%d,%d",a,b,c);
+    printf("maxium :%d\n",max(&a,&b,&c));
+    return 0;
+  }
+      
+
