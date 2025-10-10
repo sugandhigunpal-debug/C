ca845e8b0f21fb4ecb828f8be31d9e37b8c9325c
sugandhigunpal-debug <sugandhigunpal@gmail.com>
2025-10-10 17:36:44 +0530

    Add files via upload

---
+++ p3s3.cpp
@@ -0,0 +1,29 @@
+ #include<iostream>
+ using namespace std:
+  class Employee {
+public:
+    int id;
+    double salary;
+
+   
+    Employee(int i, double s) {
+        id = i;
+        salary = s;
+    }
+
+    void display() {
+        cout << "ID: " << id << ", Salary: " << salary << "\n";
+    }
+};
+
+int main() {
+    
+    Employee e1(101, 35000.50);
+    Employee e2(102, 42000.00);
+
+    cout << "Employee 1: "; e1.display();
+    cout << "Employee 2: "; e2.display();
+
+    return 0;
+}
+  
